553c83fb0fa9ac3a8fa128f5c65425826ee02127
duyandie <167690825+duyandie@users.noreply.github.com>
2025-04-13 22:51:52 +0700

    add router

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,14 @@ use Core\Route;
 
 Route::get("/", fn() => view("welcome"));
 
-Route::prefix('/users', function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::get('/{id}', [UserController::class, 'show']);
-    Route::get('/create', [UserController::class, 'create']);
-    Route::post('/store', [UserController::class, 'store']);
-});
+// Route::prefix('/users', function () {
+//     Route::get('/', [UserController::class, 'index']);
+//     Route::get('/{id}', [UserController::class, 'show']);
+//     Route::get('/create', [UserController::class, 'create']);
+//     Route::post('/store', [UserController::class, 'store']);
+// });
+require_once __DIR__ ."/acess/post.php";
+require_once __DIR__ ."/acess/category.php";
+require_once __DIR__ ."/acess/user.php";
+require_once __DIR__ ."/acess/whislist.php";
+require_once __DIR__ ."/acess/comment.php";
